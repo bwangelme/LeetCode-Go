ec72cdcfb1e15c773e19303b2b7128c3f776601d
bwangelme <bwangel.me@gmail.com>
2020-01-17 00:14:37 +0800

    Add comment for 258

--- l258/solution.go
+++ l258/solution.go
@@ -4,12 +4,15 @@ package l258
 // https://www.bwangel.me/2019/04/09/leetcode-258/
 func addDigits(num int) int {
 	if num <= 0 {
+		// 非自然数及0的情况
 		return 0
 	} else {
 		res := num % 9
 		if res == 0 {
+			// 模9等于0时，其数根为9
 			return 9
 		} else {
+			// 自然数的数根为其模9的余数
 			return res
 		}
 	}
