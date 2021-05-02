f56f25fd1220b522d0198366869c49d815a7dfdd
bwangelme <bwangel.me@gmail.com>
2021-05-02 18:41:11 +0800

    Feat: add l898

--- l1/solution.go
+++ l1/solution.go
@@ -1,15 +1,15 @@
 package l1
 
 func twoSum(nums []int, target int) []int {
-	buf_map := make(map[int]int)
+	bufMap := make(map[int]int)
 
 	for idx, num := range nums {
-		if _, ok := buf_map[num]; ok {
+		if _, ok := bufMap[num]; ok {
 			return []int{
-				buf_map[num], idx,
+				bufMap[num], idx,
 			}
 		} else {
-			buf_map[target-num] = idx
+			bufMap[target-num] = idx
 		}
 	}
 
