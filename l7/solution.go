f38dbc23babcea36cfe4afdbe3298eaf2c497bb9
bwangelme <bwangel.me@gmail.com>
2021-05-05 00:12:11 +0800

    Feat: update l7

--- l7/solution.go
+++ l7/solution.go
@@ -1,13 +1,23 @@
 package l7
 
+// 时间复杂度 log_10^(x)+1
+// 空间复杂度 变量没有随着循环增加，可以认为是常数级, O(1)
 func reverse(x int) int {
 	var res int32
 	for x != 0 {
-	    digit := x % 10
-	    // 根据 digit 的不同，Max 和 Min 会有 +-1 的浮动
-	    // 这里计算 positiveMax 和 negativeMin 还是利用 64 位正数来计算，只用32位整数计算的时候会溢出
-		var positiveMax = int32((2147483646 - digit) / 10)
-		var negativeMin = int32((-2147483647 - digit) / 10)
+		var positiveMax int32 = 214748364
+		var negativeMin int32 = -214748364
+		digit := x % 10
+		// Int32Min -2147483647, Int32Max 2147483646
+		// negativeMin = Int32Min - digit / 10
+		// positiveMax = Int32Max - digit / 10
+		// 根据 digit 的不同，Max 和 Min 会有 +-1 的浮动
+		if digit >= 3 {
+			negativeMin = -214748365
+		}
+		if digit <= -4 {
+			positiveMax = 214748365
+		}
 		if res > positiveMax || res < negativeMin {
 			return 0
 		}
