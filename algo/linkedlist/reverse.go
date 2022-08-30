679caf0e44501fa6740815a65bd8e6582501f773
xuyundong <xuyundong@douban.com>
2022-08-30 08:49:31 +0800

    update l206 solution

--- algo/linkedlist/reverse.go
+++ algo/linkedlist/reverse.go
@@ -7,6 +7,10 @@ Reverse 反转单链表
 
 1. 注意 head.next 要被设置成 nil
 2. 循环结束后，head 是 nil，prev 表示原来链表的尾部
+
+## 复杂度分析
+
+时间复杂度是 O(N), N 表示链表的长度
 */
 func Reverse[T any](head *SingleListNode[T]) (tail *SingleListNode[T]) {
 	var prev *SingleListNode[T] = nil
