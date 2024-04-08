e63b441ebb7c9dd2929c78eb89b1f8865ddc23b8
bwangelme <bwangel.me@gmail.com>
2024-04-08 22:51:21 +0800

    rename l144

---
+++ l226/solution.go
@@ -0,0 +1,76 @@
+package l226
+
+/*
+## 题目 反转二叉树
+
+给定一棵二叉树的根节点 root，请左右翻转这棵二叉树，并返回其根节点。
+
+提示：
+    树中节点数目范围在 [0, 100] 内
+    -100 <= Node.val <= 100
+
+## 复杂度分析
+
+N 是树中节点的个数
+
+### 递归解法
+
+由于会遍历每个节点，所以时间复杂度是 O(N)
+
+空间复杂度，最坏情况下树是一个链表，此时递归深度是 N, 空间复杂度是 O(N)
+
+### 栈解法
+
+因为会遍历每个节点，时间复杂度是 O(N)
+
+空间复杂度，最坏情况下树是满二叉树，栈的大小是树的深度，空间复杂度是 O(logN)
+
+设满二叉树的深度是t, 节点总数是 N
+
+N = 2^t - 1
+t = log_2{N+1}
+*/
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// mirrorTreeRecursion
+// 递归解法
+func mirrorTreeRecursion(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	tmp := root.Left
+	root.Left = mirrorTreeRecursion(root.Right)
+	root.Right = mirrorTreeRecursion(tmp)
+
+	return root
+}
+
+func mirrorTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	stack := []*TreeNode{root}
+	for len(stack) > 0 {
+		// stack pop
+		top := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		if top.Left != nil {
+			stack = append(stack, top.Left)
+		}
+		if top.Right != nil {
+			stack = append(stack, top.Right)
+		}
+
+		top.Left, top.Right = top.Right, top.Left
+	}
+
+	return root
+}
