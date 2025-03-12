6d4d52215267b6db9b7df9ee550518d973f469a5
bwangel <bwangelme@qq.com>
2025-03-13 00:53:33 +0800

    Update final_solution_test.go

--- l373/final_solution_test.go
+++ l373/final_solution_test.go
@@ -87,11 +87,11 @@ func readArrFromFile(filename string) (res []int, err error) {
 func Test_kSmallestPairsV3(t *testing.T) {
 	var res [][]int
 
-	//res = kSmallestPairsV3([]int{1, 7, 11}, []int{2, 4, 6}, 3)
-	//assert.Equal(t, res, [][]int{{1, 2}, {1, 4}, {1, 6}})
-	//
-	//res = kSmallestPairsV3([]int{1, 1, 2}, []int{1, 2, 3}, 2)
-	//assert.Equal(t, res, [][]int{{1, 1}, {1, 1}})
+	res = kSmallestPairsV3([]int{1, 7, 11}, []int{2, 4, 6}, 3)
+	assert.Equal(t, res, [][]int{{1, 2}, {1, 4}, {1, 6}})
+
+	res = kSmallestPairsV3([]int{1, 1, 2}, []int{1, 2, 3}, 2)
+	assert.Equal(t, res, [][]int{{1, 1}, {1, 1}})
 
 	num1, err := readArrFromFile("num1.txt")
 	assert.Nil(t, err)
