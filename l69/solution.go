32dc66a649c8216d1357e595fa9100f08d58d893
bwangelme <bwangel.me@gmail.com>
2025-04-23 23:28:23 +0800

    update l69

--- l69/solution.go
+++ l69/solution.go
@@ -41,3 +41,60 @@ func mySqrt(x int) int {
 
 	return right
 }
+
+/*
+## 题目描述
+
+给你一个非负整数 x ，计算并返回 x 的 算术平方根 。
+
+由于返回类型是整数，结果只保留 整数部分 ，小数部分将被 舍去 。
+
+注意：不允许使用任何内置指数函数和算符，例如 pow(x, 0.5) 或者 x ** 0.5 。
+
+示例 1：
+
+输入：x = 4
+输出：2
+示例 2：
+
+输入：x = 8
+输出：2
+解释：8 的算术平方根是 2.82842..., 由于返回类型是整数，小数部分将被舍去。
+
+提示：
+
+0 <= x <= 231 - 1
+
+## 解题思路
+
+从 1-x 进行二分查找，查找 target^2 > n && (target+1) ^ 2 < n
+
+如果输入的 x 是0, 那么 left > right, 直接返回0就好
+
+## 复杂度分析
+
+N = x 的大小
+
+空间复杂度 O(1)
+时间复杂度 O(logN)
+*/
+func mySqrtV2(x int) int {
+	var (
+		left  = 1
+		right = x
+	)
+	for left <= right {
+		var mid = (left + right) / 2
+		if mid <= x/mid {
+			if (mid + 1) > x/(mid+1) {
+				return mid
+			}
+
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+
+	}
+	return 0
+}
